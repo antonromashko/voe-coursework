ca9a604ab5150b49bed19f11b20acf1a8fcdc7fe
Anton Romashko <anton.romashko@pm.bet>
2021-01-14 23:38:22 +0200

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,9 @@
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/vue-coursework/'
+    : '/',
     css: {
       requireModuleExtension: true,
         sourceMap: true,
-        // loaderOptions: {
-        //     sass: {
-        //         additionalData: `@import "~bootstrap/scss/bootstrap";`
-        //     }
-        // }
     }
 }
