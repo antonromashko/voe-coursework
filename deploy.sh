9abb9a3eacc8c49cc6fab6fa1788cbef5b6be300
Anton Romashko <anton.romashko@pm.bet>
2021-01-15 00:14:11 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ git add -A
 git commit -m 'deploy'
 
 # если вы публикуете по адресу https://<USERNAME>.github.io
-git push -f git@github.com:<USERNAME>/antonromashko.github.io.git master
+git push --set-upstream git@github.com:antonromashko/vue-project.git master
 
 
 cd -
