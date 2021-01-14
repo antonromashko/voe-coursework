25e934b546c32c8a845f259eb147ea964d5b7135
Anton Romashko <anton.romashko@pm.bet>
2021-01-15 00:20:48 +0200

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue-coursework/'
+    ? '/vue-project/'
     : '/',
     css: {
       requireModuleExtension: true,
