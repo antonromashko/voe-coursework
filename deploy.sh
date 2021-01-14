08c15649a9973388962b669d1521bb60c14aea28
Anton Romashko <anton.romashko@pm.bet>
2021-01-15 00:23:39 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -9,13 +9,14 @@ npm run build
 # переход в каталог сборки
 cd dist
 
+git pull
 
 git init
 git add -A
 git commit -m 'deploy'
 
 # если вы публикуете по адресу https://<USERNAME>.github.io
-git push --set-upstream git@github.com:antonromashko/vue-project.git master
+git push -f git@github.com:antonromashko/vue-project.git master
 
 
 cd -
